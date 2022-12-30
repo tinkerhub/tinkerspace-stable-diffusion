25bbdc2f2ad9f06e2e985ef11a9af983e48e46c8
Saneem <xaneem@gmail.com>
2022-12-30 12:26:08 +0000

    New page added

--- pages/new.js
+++ pages/new.js
@@ -2,20 +2,25 @@ import { useState } from 'react'
 import { insertPrompt } from '../lib/db';
 export default function CreateForm() {
     const [textPrompt, setTextPrompt] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
     return (
-        <div className="container p-4 flex items-start space-x-4">
+        <div className="flex flex-col items-center justify-center min-h-screen py-2 px-2">
+            {!isSubmitting && (
             <div>
                 <label htmlFor="comment" className="block text-sm font-medium text-gray-700">
-                    Text Prompt
+                    What would you like to see?
                 </label>
-                <div className="mt-1">
+                <div className="mt-3 mb-5">
                     <textarea
                         onChange={(e) => setTextPrompt(e.target.value)}
+                        value={textPrompt}
                         rows={4}
                         name="comment"
                         id="comment"
-                        className="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"
+                        className="shadow-sm focus:ring-blue-500 focus:border-blue-500 mt-1 block w-full sm:text-sm border-gray-300 rounded-md border-2 p-2"
                         defaultValue={''}
+                        placeholder="Type here..."
                     />
                 </div>
                 <button onClick={
@@ -23,12 +28,36 @@ export default function CreateForm() {
                         insertPrompt({
                             text: textPrompt,
                         })
-                    }
-                } className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Submit
-                </button>
-            </div>
-        </div>
+                        setIsSubmitting(true);
 
+                        setTextPrompt('');
+                    }
+                }
+                disabled={textPrompt.length < 10}
+                className={
+                    textPrompt.length < 10
+                        ? 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded opacity-50 cursor-not-allowed w-full'
+                    : 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full'
+                }
+                >
+                    Generate
+                </button>
+                <p className="mt-2 text-sm text-gray-500">
+                    Please explain what you'd like to see in detail. <br/>For example, "A monkey sitting on top of a train eating a banana"
+                </p>
+            </div>
+            )}
+            {isSubmitting && (
+                <div>
+                    <p className="text-2xl">Check on the TinkerSpace TV!</p>
+                    <p className="mt-2 text-sm text-gray-500">
+                        After your image is generated, it will be shown on the TinkerSpace TV.
+                    </p>
+                    <p className="mt-10 text-sm text-gray-500">
+                        Scan QR again or refresh this page to try again.
+                    </p>
+                </div>
+            )}
+        </div>
     )
 }
