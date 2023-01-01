f9632882ced10b0cb52967de13922ac450bff7c4
ajnas <mailajnaskt@gmail.com>
2023-01-01 19:12:27 +0530

    Simply pull latest items from completed queue

--- hooks/index.js
+++ hooks/index.js
@@ -1,13 +1,13 @@
 import { ref, orderByChild, limitToLast, query, equalTo, startAt, limitToFirst, onValue, get } from 'firebase/database';
 import { useList, useListVals } from 'react-firebase-hooks/database';
 import React, { use, useEffect, useState } from 'react'
-import { queueRef } from '@lib/firebase';
+import { queueRef, completedRef } from '@lib/firebase';
 
-export function useItems({
+export function completedItems({
     limit = 6,
 }) {
     const latestRef = query(
-        queueRef,
+        completedRef,
         orderByChild('completed_at'),
         limitToLast(limit)
     );
@@ -16,7 +16,7 @@ export function useItems({
 }
 
 export function useRecentImages() {
-    const [items, loading, error] = useItems({ limit: 8 });
+    const [items, loading, error] = completedItems({ limit: 8 });
     const [images, setImages] = useState([]);
     useEffect(() => {
         if(items.length > 0) {
@@ -32,60 +32,21 @@ export function useRecentImages() {
 }
 
 export function useRecentQueue() {
-    const latestRef = query(
+    const [items, loading, error] = useListVals(query(
         queueRef,
         orderByChild('timestamp'),
-        limitToLast(6)
-    );
-    const [items, loading, error] = useListVals(latestRef);
-    const [reverseItems, setReverseItems] = useState([]);
-
-    useEffect(() => {
-        if (items.length > 0) {
-            setReverseItems(items.reverse());
-        }
-    }, [items]);
-    return [reverseItems, loading, error];
+        limitToFirst(6)
+    ));
+    return [items, loading, error];
 }
 
 export function useLatestItem() {
+    const [items, loading, error] = completedItems({ limit: 1 })
     const [item, setItem] = React.useState(null);
-    async function sync(params) {
-        const latestRef = query(
-            queueRef,
-            orderByChild('expiry_at'),
-            startAt(new Date().getTime()),
-            limitToFirst(1)
-        );
-
-        const snapshot = await get(latestRef);
-        const data = snapshot.val();
-        if (data != null)
-           { setItem(Object.values(data)[0])}
-        else {
-            const lastItemRef = query(
-                queueRef,
-                orderByChild('expiry_at'),
-                limitToLast(1)
-            );
-    
-            const lastItemsnapshot = await get(lastItemRef);
-            const lastItemdata = lastItemsnapshot.val();
-            if (lastItemdata != null)
-            { setItem(Object.values(lastItemdata)[0])}
-        }
-    }
-
     useEffect(() => {
-        const interval = setInterval(() => {
-            // setCount(count + 1);
-            sync();
-        }, 1000);
-
-        return () => clearInterval(interval);
-    }, []);
-
-    const loading = item == null;
-    return [item, loading];
-
+        if (items.length > 0) {
+            setItem(items[0]);
+        }
+    }, [items]);
+    return [item, loading]
 }
