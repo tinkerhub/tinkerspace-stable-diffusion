aaeeb55b237c8d09214378c444fddf238164ef23
Abhijith v <abhijithababhijith@gmail.com>
2023-01-08 14:31:53 +0530

    added image slider when queue is ideal

--- components/ImageSlide.js
+++ components/ImageSlide.js
@@ -1,25 +1,62 @@
-import { useLatestItem } from '@hooks/index';
-import React from 'react'
+import { useLatestItem } from "@hooks/index";
+import React, { useEffect, useState } from "react";
+import ImageAutoSlider from "@components/ImageAutoSlider";
+
+const SHOW_IMAGE_SLIDER_AFTER = 5; // minutes
+
+/**
+ *  Function to check if the image slider should be shown
+ * @param {Timestamp} lastCompletedAt last completed image timestamp
+ * @returns Boolean
+ */
+const showImageAutoSlider = (lastCompletedAt) => {
+  if (!lastCompletedAt) return false;
+  const milliseconds = new Date() - new Date(lastCompletedAt);
+  const minutes = Math.floor(milliseconds / 60000);
+  // Show the image slider based on the time
+  if (minutes >= SHOW_IMAGE_SLIDER_AFTER) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
 export default function ImageSlide() {
-    const [latestItem, loadingLatest, errorLatest] = useLatestItem();
-    return (
-        <div>
-            {loadingLatest && <div className='text-lg p-5'>Loading ...</div>}
-            {errorLatest && <div className='text-lg p-5'>Error</div>}
-            {latestItem && (
-                <img
-                    src={latestItem?.url}
-                    alt={latestItem?.text}
-                    className='w-full h-screen object-cover'
-                />
-            )}
-            {latestItem && (
-                <div className='absolute w-full bottom-0 p-5 bg-black bg-opacity-80'>
-                    {/* Make the text of size 60px in Tailwind*/}
-                    <p className='text-white text-3xl'>{latestItem?.text}</p>
-                </div>
-            )}
+  const [latestItem, loadingLatest, errorLatest] = useLatestItem();
+  const [showSlider, setShowSlider] = useState(false);
+
+  useEffect(() => {
+    if (latestItem?.completed_at) {
+      setShowSlider(showImageAutoSlider(latestItem?.completed_at));
+    }
+    // useEffect will only call on the update of lastItem, so we need to check after the time again
+    const timeout = setTimeout(() => {
+      setShowSlider(showImageAutoSlider(latestItem?.completed_at));
+    }, 60000 * SHOW_IMAGE_SLIDER_AFTER); // call the effect again after SHOW_IMAGE_SLIDER_AFTER minutes
+    return () => clearTimeout(timeout);
+  }, [latestItem]);
+
+  if (showSlider) {
+    return <ImageAutoSlider />;
+  }
+
+  return (
+    <div>
+      {loadingLatest && <div className="text-lg p-5">Loading ...</div>}
+      {errorLatest && <div className="text-lg p-5">Error</div>}
+      {latestItem && (
+        <img
+          src={latestItem?.url}
+          alt={latestItem?.text}
+          className="w-full h-screen object-cover"
+        />
+      )}
+      {latestItem && (
+        <div className="absolute w-full bottom-0 p-5 bg-black bg-opacity-80">
+          {/* Make the text of size 60px in Tailwind*/}
+          <p className="text-white text-3xl">{latestItem?.text}</p>
         </div>
-    )
+      )}
+    </div>
+  );
 }
--- hooks/index.js
+++ hooks/index.js
@@ -15,11 +15,13 @@ export function useCompletedItems({
     return [items, loading, error];
 }
 
-export function useRecentImages() {
-    const [items, loading, error] = useCompletedItems({ limit: 8 });
+export function useRecentImages({
+    limit = 8
+}={}) {
+    const [items, loading, error] = useCompletedItems({ limit: limit });
     const [images, setImages] = useState([]);
     useEffect(() => {
-        if(items.length > 0) {
+        if(items?.length > 0) {
             // remove last item 
             const newImages = items.slice(0, -1);
             // reverse order
@@ -44,7 +46,7 @@ export function useLatestItem() {
     const [items, loading, error] = useCompletedItems({ limit: 1 })
     const [item, setItem] = React.useState(null);
     useEffect(() => {
-        if (items.length > 0) {
+        if (items?.length > 0) {
             setItem(items[0]);
         }
     }, [items]);
